6e7b9130c87975de4f9d467442e3863af3e7518e
Nikolas Lep <nikolaslep_@hotmail.com>
2023-02-09 16:01:53 +0200

    Added first iteration of User entity

---
+++ src/main/java/com/twitter/domain/User.java
@@ -0,0 +1,75 @@
+package com.twitter.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(name = "username", length = 30, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", length = 30, nullable = false)
+    private String password;
+
+    @Column(name = "email", length = 30, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "followersCount")
+    private Integer followersCount;
+
+    @Column(name = "followingCount")
+    private Integer followingCount;
+
+    public User() { }
+
+    public User (String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getFollowersCount() {
+        return followersCount;
+    }
+
+    public void setFollowersCount(Integer followersCount) {
+        this.followersCount = followersCount;
+    }
+
+    public Integer getFollowingCount() {
+        return followingCount;
+    }
+
+    public void setFollowingCount(Integer followingCount) {
+        this.followingCount = followingCount;
+    }
+}
