e6c7af306c9b27a0240f3f4410661bc662128659
Hercules Chrysanthos <hercsma@hotmail.com>
2023-02-17 02:04:49 +0200

    Refactor Reply to extends Post
    Signed-off-by: Hercules Chrysanthos <hercsma@hotmail.com>

--- src/main/java/com/twitter/domain/Reply.java
+++ src/main/java/com/twitter/domain/Reply.java
@@ -3,19 +3,39 @@ package com.twitter.domain;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "replies")
-public class Reply {
-    @Id
-    @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+@DiscriminatorValue("REPLY")
+public class Reply extends Post {
 
     @Column(name = "content", length = 50, nullable = false)
-    private String content;
+    private TweetBody content;
 
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE })
-    @JoinColumn(name = "tweetId")
-    private Tweet tweet;
+    @JoinColumn(name = "postId")
+    private Post post;
 
-    public Reply() { }
+    public Reply() {
+
+    }
+
+    public Reply(User user, TweetBody content, Post post) {
+        super(user);
+        this.content = content;
+        this.post = post;
+    }
+
+    public TweetBody getContent() {
+        return content;
+    }
+
+    public void setContent(TweetBody content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
