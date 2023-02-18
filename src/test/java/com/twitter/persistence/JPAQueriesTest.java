a83992af247966701edc426850fb0324d3554933
Mike <mathanasopoulos@hotmail.com>
2023-02-18 18:14:52 +0200

    Delete unnecessary assertions

--- src/test/java/com/twitter/persistence/JPAQueriesTest.java
+++ src/test/java/com/twitter/persistence/JPAQueriesTest.java
@@ -36,14 +36,5 @@ public class JPAQueriesTest {
         List<Tweet> results = query.getResultList();
 
         Assertions.assertEquals(1,results.size());
-
-        Tweet tweet = results.get(0);
-
-       // Assertions.assertEquals(tweet.getLikeCount(), 0);
-       // Assertions.assertEquals(tweet.getRetweetCount(), 0);
-        Assertions.assertEquals(tweet.getTweetBody(), "My first tweet");
-        Assertions.assertEquals(tweet.getUser().getUsername(), "nikolaslepidas");
-        Assertions.assertEquals(tweet.getUser().getPassword(), "1234");
-        Assertions.assertEquals(tweet.getUser().getEmail(), "nl@gmail.com");
     }
 }
