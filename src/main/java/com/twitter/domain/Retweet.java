2473c44edc00ded9ef022b7cf9c78774044d8cf4
Hercules Chrysanthos <hercsma@hotmail.com>
2023-02-18 17:05:23 +0200

    Rename post to retweet
    Signed-off-by: Hercules Chrysanthos <hercsma@hotmail.com>

--- src/main/java/com/twitter/domain/Retweet.java
+++ src/main/java/com/twitter/domain/Retweet.java
@@ -9,22 +9,22 @@ public class Retweet extends Post{
     // if you delete a retweet, you don't delete the post but only you update its set of retweets
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE })
     @JoinColumn(name = "postId")
-    private Post post;
+    private Post retweet;
 
     public Retweet(){
 
     }
 
-    public Retweet(User user, TweetBody tweetBody, Post post) {
-        super(user);
-        this.post = post;
+//    public Retweet(User user, TweetBody tweetBody, Post post) {
+//        super(user);
+//        this.retweet = post;
+//    }
+
+    public Post getRetweet() {
+        return retweet;
     }
 
-    public Post getPost() {
-        return post;
-    }
-
-    public void setPost(Post post) {
-        this.post = post;
+    public void setRetweet(Post retweet) {
+        this.retweet = retweet;
     }
 }
