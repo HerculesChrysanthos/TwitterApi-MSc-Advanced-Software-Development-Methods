11e9740f60d7b71efe29025585e3336fa9594669
Nikolas Lep <nikolaslep_@hotmail.com>
2023-02-19 14:54:53 +0200

    Added new Initializer

--- src/main/java/com/twitter/persistence/Initializer.java
+++ src/main/java/com/twitter/persistence/Initializer.java
@@ -27,23 +27,47 @@ public class Initializer  {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        User user1 = new User("herculesCh","1234",new DateOfBirth(1,7,1999),new EmailAddress("h@gmail.com"));
-        Tweet tweet1 = new Tweet("kalispera kai kali vradia");
+        User user0 = new User("user0", "0", new DateOfBirth(1,1,2000), new EmailAddress("email0@gmail.com"));
+        User user1 = new User("user1", "1", new DateOfBirth(1,1,2001), new EmailAddress("email1@gmail.com"));
+        User user2 = new User("user2","2",new DateOfBirth(1,7,2002),new EmailAddress("email2@gmail.com"));
+        User user3 = new User("user3","3",new DateOfBirth(1,7,2003),new EmailAddress("email3@gmail.com"));
+        User user4 = new User("user4","4",new DateOfBirth(1,7,2004),new EmailAddress("email4@gmail.com"));
+        User user5 = new User("user5","5",new DateOfBirth(1,7,2005),new EmailAddress("email5@gmail.com"));
+        User user6 = new User("user6","6",new DateOfBirth(1,7,2006),new EmailAddress("email6@gmail.com"));
 
-        User user2 = new User("nikolaslepidas","1234",new DateOfBirth(1,7,1999),new EmailAddress("nl@gmail.com"));
-
-        User user3 = new User("mikeathanasopoulos","1234",new DateOfBirth(1,7,1999),new EmailAddress("mike@gmail.com"));
-
-        user1.setFollowing(user2);
-        user1.setFollowing(user3);
+        Tweet tweet1 = new Tweet(new TweetBody("This is tweet #1"));
+        Tweet tweet2 = new Tweet(new TweetBody("This is tweet #2"));
+        Tweet tweet3 = new Tweet(new TweetBody("This is tweet #3"));
+        Tweet tweet4 = new Tweet(new TweetBody("This is tweet #4"));
+        Tweet tweet5 = new Tweet(new TweetBody("This is tweet #5"));
+        Tweet tweet6 = new Tweet(new TweetBody("This is tweet #6"));
 
         tweet1.setUser(user1);
+        tweet2.setUser(user2);
+        tweet3.setUser(user2);
+        tweet4.setUser(user2);
+        tweet5.setUser(user3);
+        tweet6.setUser(user4);
 
+        user1.setFollowing(user2);
+        user2.setFollowing(user3);
+        user2.setFollowing(user4);
+        user2.setFollowing(user5);
+
+        em.persist(user0);
         em.persist(user1);
         em.persist(user2);
         em.persist(user3);
+        em.persist(user4);
+        em.persist(user5);
+        em.persist(user6);
 
         em.persist(tweet1);
+        em.persist(tweet2);
+        em.persist(tweet3);
+        em.persist(tweet4);
+        em.persist(tweet5);
+        em.persist(tweet6);
 
         tx.commit();
         em.close();
