ff57ed898d67c3be61a999e27e1e53d6810a0382
Hercules Chrysanthos <hercsma@hotmail.com>
2023-02-22 20:11:27 +0200

    Move user tests into UserTest
    Signed-off-by: Hercules Chrysanthos <hercsma@hotmail.com>

--- src/test/java/com/twitter/persistence/JPAQueriesTest.java
+++ src/test/java/com/twitter/persistence/JPAQueriesTest.java
@@ -38,52 +38,5 @@ public class JPAQueriesTest {
         Assertions.assertEquals(7,results.size());
     }
 
-    @Test
-    public void getFollowingUsers() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
 
-        Assertions.assertEquals(7, users.size());
-
-        Assertions.assertEquals(0, users.get(0).getFollowing().size());
-
-        Assertions.assertTrue(users.get(1).getFollowing().contains(users.get(2)));
-        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(3)));
-        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(4)));
-        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(5)));
-
-        Assertions.assertEquals(0, users.get(3).getFollowing().size());
-        Assertions.assertEquals(0, users.get(4).getFollowing().size());
-        Assertions.assertEquals(0, users.get(5).getFollowing().size());
-        Assertions.assertEquals(0, users.get(6).getFollowing().size());
-
-        Assertions.assertFalse(users.get(1).getFollowing().contains(users.get(3)));
-    }
-
-    @Test
-    public void UserCanNotFollowAFollowingUser() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
-
-        Assertions.assertFalse(users.get(1).followUser(users.get(2)));
-    }
-    @Test
-    public void userCanFollowANonFollowingUser() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
-
-        Assertions.assertTrue(users.get(0).followUser(users.get(1)));
-    }
-
-    @Test
-    public void userCanNotFollowHimself() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
-
-        Assertions.assertFalse(users.get(1).followUser(users.get(1)));
-    }
 }
