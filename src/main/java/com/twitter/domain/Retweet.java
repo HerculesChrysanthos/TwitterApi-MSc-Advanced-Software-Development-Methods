c97be9efb01fa542f4b6d08cc43a8e21d0ed9f33
Nikolas Lep <nikolaslep_@hotmail.com>
2023-02-23 01:49:10 +0200

    Removed @ManyToOne association from Retweet.java and now we have only the @OneToMany association in Post.java

--- src/main/java/com/twitter/domain/Retweet.java
+++ src/main/java/com/twitter/domain/Retweet.java
@@ -5,19 +5,9 @@ import javax.persistence.*;
 @Entity
 @DiscriminatorValue("RETWEET")
 public class Retweet extends Post{
-    // if you delete a retweet, you don't delete the post but only you update its set of retweets
-    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "postId")
-    private Post originalPost;
-
     public Retweet() { }
 
-    public Retweet(User user, Post originalPost) {
+    public Retweet(User user) {
         super(user);
-        this.originalPost = originalPost;
-    }
-
-    public Post getRetweet() {
-        return originalPost;
     }
 }
