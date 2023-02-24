2842ddf319c836ec930590e4567e04369c2a7b4a
Hercules Chrysanthos <hercsma@hotmail.com>
2023-02-24 22:51:53 +0200

    Refactor UserTest's into unit tests and comment integration
    Signed-off-by: Hercules Chrysanthos <hercsma@hotmail.com>

--- src/test/java/com/twitter/domain/UserTest.java
+++ src/test/java/com/twitter/domain/UserTest.java
@@ -48,50 +48,83 @@ public class UserTest {
 
     @Test
     public void testGetFollowingUsers() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
+//        EntityManager em = JPAUtil.getCurrentEntityManager();
+//        Query query = em.createQuery("select user from User user");
+//        List<User> users = query.getResultList();
+//
+//        Assertions.assertEquals(7, users.size());
+//
+//        Assertions.assertEquals(0, users.get(0).getFollowing().size());
+//
+//        Assertions.assertTrue(users.get(1).getFollowing().contains(users.get(2)));
+//        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(3)));
+//        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(4)));
+//        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(5)));
+//
+//        Assertions.assertEquals(0, users.get(3).getFollowing().size());
+//        Assertions.assertEquals(0, users.get(4).getFollowing().size());
+//        Assertions.assertEquals(0, users.get(5).getFollowing().size());
+//        Assertions.assertEquals(0, users.get(6).getFollowing().size());
+//
+//        Assertions.assertFalse(users.get(1).getFollowing().contains(users.get(3)));
 
-        Assertions.assertEquals(7, users.size());
+        User user1 = new User("user1", "1", new DateOfBirth(1,1,2001), new EmailAddress("email1@gmail.com"));
+        User user2 = new User("user2","2",new DateOfBirth(1,7,2002),new EmailAddress("email2@gmail.com"));
+        User user3 = new User("user3","3",new DateOfBirth(1,7,2003),new EmailAddress("email3@gmail.com"));
+        User user4 = new User("user4","4",new DateOfBirth(1,7,2004),new EmailAddress("email4@gmail.com"));
 
-        Assertions.assertEquals(0, users.get(0).getFollowing().size());
+        user1.setFollowing(user2);
+        user2.setFollowing(user3);
+        user2.setFollowing(user4);
 
-        Assertions.assertTrue(users.get(1).getFollowing().contains(users.get(2)));
-        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(3)));
-        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(4)));
-        Assertions.assertTrue(users.get(2).getFollowing().contains(users.get(5)));
+        Assertions.assertTrue(user1.getFollowing().contains(user2));
+        Assertions.assertTrue(user2.getFollowing().contains(user3));
+        Assertions.assertTrue(user2.getFollowing().contains(user4));
 
-        Assertions.assertEquals(0, users.get(3).getFollowing().size());
-        Assertions.assertEquals(0, users.get(4).getFollowing().size());
-        Assertions.assertEquals(0, users.get(5).getFollowing().size());
-        Assertions.assertEquals(0, users.get(6).getFollowing().size());
-
-        Assertions.assertFalse(users.get(1).getFollowing().contains(users.get(3)));
+        Assertions.assertEquals(0, user3.getFollowing().size());
+        Assertions.assertEquals(1, user1.getFollowing().size());
+        Assertions.assertEquals(2, user2.getFollowing().size());
+        Assertions.assertFalse(user1.getFollowing().contains(user3));
     }
 
     @Test
     public void testUserCanNotFollowAFollowingUser() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
+//        EntityManager em = JPAUtil.getCurrentEntityManager();
+//        Query query = em.createQuery("select user from User user");
+//        List<User> users = query.getResultList();
+//
+//        Assertions.assertFalse(users.get(1).followUser(users.get(2)));
 
-        Assertions.assertFalse(users.get(1).followUser(users.get(2)));
+        User user1 = new User("user1", "1", new DateOfBirth(1,1,2001), new EmailAddress("email1@gmail.com"));
+        User user2 = new User("user2","2",new DateOfBirth(1,7,2002),new EmailAddress("email2@gmail.com"));
+        user1.setFollowing(user2);
+
+        Assertions.assertFalse(user1.followUser(user2));
     }
+
     @Test
     public void testUserCanFollowANonFollowingUser() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
+//        EntityManager em = JPAUtil.getCurrentEntityManager();
+//        Query query = em.createQuery("select user from User user");
+//        List<User> users = query.getResultList();
+//
+//        Assertions.assertTrue(users.get(0).followUser(users.get(1)));
 
-        Assertions.assertTrue(users.get(0).followUser(users.get(1)));
+        User user1 = new User("user1", "1", new DateOfBirth(1,1,2001), new EmailAddress("email1@gmail.com"));
+        User user3 = new User("user3","3",new DateOfBirth(1,7,2003),new EmailAddress("email3@gmail.com"));
+
+        Assertions.assertTrue(user1.followUser(user3));
     }
 
     @Test
     public void testUserCanNotFollowHimself() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select user from User user");
-        List<User> users = query.getResultList();
+//        EntityManager em = JPAUtil.getCurrentEntityManager();
+//        Query query = em.createQuery("select user from User user");
+//        List<User> users = query.getResultList();
+//
+//        Assertions.assertFalse(users.get(1).followUser(users.get(1)));
+        User user1 = new User("user1", "1", new DateOfBirth(1,1,2001), new EmailAddress("email1@gmail.com"));
 
-        Assertions.assertFalse(users.get(1).followUser(users.get(1)));
+        Assertions.assertFalse(user1.followUser(user1));
     }
 }
