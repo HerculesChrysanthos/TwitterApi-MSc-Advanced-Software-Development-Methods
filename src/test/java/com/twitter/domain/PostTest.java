55b9b0674046df317b2858292a8e48e83279ba42
Mike <mathanasopoulos@hotmail.com>
2023-02-24 15:09:01 +0200

    Add JUnit test to testAddRetweetToRetweet()

--- src/test/java/com/twitter/domain/PostTest.java
+++ src/test/java/com/twitter/domain/PostTest.java
@@ -155,15 +155,32 @@ public class PostTest {
     }
 
     /**
-     * Done
+     * Done with JUnit tests
      */
     @Test
     public void testAddRetweetToRetweet() {
-        EntityManager em = JPAUtil.getCurrentEntityManager();
-        Query query = em.createQuery("select post from Post post where TYPE(post) = Retweet");
-        List<Post> retweets = query.getResultList();
-        Assertions.assertEquals( 3, retweets.size());
-        Assertions.assertEquals( 1, retweets.get(0).getRetweets().size());
+//        EntityManager em = JPAUtil.getCurrentEntityManager();
+//        Query query = em.createQuery("select post from Post post where TYPE(post) = Retweet");
+//        List<Post> retweets = query.getResultList();
+//        Assertions.assertEquals( 3, retweets.size());
+//        Assertions.assertEquals( 1, retweets.get(0).getRetweets().size());
+        Tweet tweet1 = new Tweet();
+        tweet1.setTweetBody(new TweetBody("This is tweet #1"));
+
+        User user1 = new User("user1", "1", new DateOfBirth(1,1,2001), new EmailAddress("email1@gmail.com"));
+        User user2 = new User("user2","2",new DateOfBirth(1,7,2002),new EmailAddress("email2@gmail.com"));
+        User user3 = new User("user3","3",new DateOfBirth(1,7,2003),new EmailAddress("email3@gmail.com"));
+
+        tweet1.setUser(user1);
+
+        Retweet retweet1 = new Retweet(user2);
+        tweet1.addRetweet(retweet1);
+
+        Retweet retweet2 = new Retweet(user3);
+        retweet1.addRetweet(retweet2);
+
+        Assertions.assertEquals( 1, tweet1.getRetweets().size());
+        Assertions.assertEquals( 1, retweet1.getRetweets().size());
     }
 
     /**
