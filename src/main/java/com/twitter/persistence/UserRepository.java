e30ebc1cc2f22fb123c76d5566a9463f6cb778bf
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-04 18:23:25 +0200

    Add UserRepository
    Signed-off-by: Hercules Chrysanthos <hercsma@hotmail.com>

---
+++ src/main/java/com/twitter/persistence/UserRepository.java
@@ -0,0 +1,10 @@
+package com.twitter.persistence;
+
+import com.twitter.domain.User;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+
+public class UserRepository implements PanacheRepositoryBase<User, Integer> {
+
+    
+
+}
