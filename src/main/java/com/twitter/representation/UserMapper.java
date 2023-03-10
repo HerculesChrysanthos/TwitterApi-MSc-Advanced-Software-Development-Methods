86abc5dc66700d6129f4099bdf60e2afe821127f
Nikolas Lep <nikolaslep_@hotmail.com>
2023-03-10 18:19:01 +0200

    Added UserMapper and the respective test (not finalized)

--- src/main/java/com/twitter/representation/UserMapper.java
+++ src/main/java/com/twitter/representation/UserMapper.java
@@ -6,18 +6,17 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import java.util.List;
+import java.util.Set;
 
 @Mapper(componentModel = "cdi",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public abstract class UserMapper {
 
-    @Mapping(target = "username")
-//    @Mapping(target = "email.address")
+    @Mapping(target = "password", ignore = true)
     public abstract UserRepresentation toRepresentation(User user);
-
     @Mapping(target = "following", ignore = true)
     public abstract  User toModel(UserRepresentation representation);
 
     public abstract List<UserRepresentation> toRepresentationList(List<User> users);
-
+    public abstract List<UserRepresentation> toRepresentationSet(Set<User> users);
 }
