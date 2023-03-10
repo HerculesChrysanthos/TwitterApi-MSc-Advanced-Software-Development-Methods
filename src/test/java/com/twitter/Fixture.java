d87c7ac04f0aae8894feebc7e0994cd42e2b122a
Nikolas Lep <nikolaslep_@hotmail.com>
2023-03-10 18:25:32 +0200

    Added Fixture.java in order to attempt the UserResourceTest. Copied the logic from Library example.

---
+++ src/test/java/com/twitter/Fixture.java
@@ -0,0 +1,10 @@
+package com.twitter;
+
+public class Fixture {
+    public static class Users {
+        public static final int NIKOLAS_ID = 1;
+        public static final int MICHALIS_ID = 2;
+        public static final int COUNT = 2;
+    }
+    public static String API_ROOT  = "http://localhost:8081";
+}
