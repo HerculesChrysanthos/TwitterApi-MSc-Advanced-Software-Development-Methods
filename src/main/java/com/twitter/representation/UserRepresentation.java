2963f631bd345c9bdce572076b1529f09f218f3b
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-11 19:27:37 +0200

    Refactor email type on UserRepresentation

--- src/main/java/com/twitter/representation/UserRepresentation.java
+++ src/main/java/com/twitter/representation/UserRepresentation.java
@@ -7,6 +7,6 @@ public class UserRepresentation {
     public Integer id;
     public String username;
     public String password;
-    public EmailAddressRepresentation email;
+    public String email;
     public DateOfBirthRepresentation dateOfBirth;
 }
