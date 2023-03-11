828ff2471eb107c832d747ccce51afe5712a602e
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-11 19:16:30 +0200

    Add toModel to DateOfBirthMapper

--- src/main/java/com/twitter/representation/DateOfBirthMapper.java
+++ src/main/java/com/twitter/representation/DateOfBirthMapper.java
@@ -7,5 +7,7 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "cdi",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public abstract class DateOfBirthMapper {
-        public abstract DateOfBirthRepresentation toRepresentation(DateOfBirth dateOfBirth);
+
+    public abstract DateOfBirthRepresentation toRepresentation(DateOfBirth dateOfBirth);
+    public abstract DateOfBirth toModel(DateOfBirthRepresentation representation);
 }
