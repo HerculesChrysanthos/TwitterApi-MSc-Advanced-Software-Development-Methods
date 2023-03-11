075dede19819c37f77e0a7784f0087feafb1f80e
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-11 21:42:50 +0200

    Delete EmailAddressMapper and EmailAddressRepresentation

--- src/main/java/com/twitter/representation/EmailAddressRepresentation.java
+++
@@ -1,8 +0,0 @@
-package com.twitter.representation;
-
-import io.quarkus.runtime.annotations.RegisterForReflection;
-
-@RegisterForReflection
-public class EmailAddressRepresentation {
-    public String email;
-}
