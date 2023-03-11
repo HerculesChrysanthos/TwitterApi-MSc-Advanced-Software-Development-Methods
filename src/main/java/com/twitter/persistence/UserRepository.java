254219b3c0a9b61359e074205cf5de55700b43d7
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-11 21:44:27 +0200

    Add findByEmail repository function

--- src/main/java/com/twitter/persistence/UserRepository.java
+++ src/main/java/com/twitter/persistence/UserRepository.java
@@ -1,5 +1,6 @@
 package com.twitter.persistence;
 
+import com.twitter.domain.EmailAddress;
 import com.twitter.domain.User;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
@@ -13,6 +14,18 @@ public class UserRepository implements PanacheRepositoryBase<User, Integer> {
 
     public User findByUsername(String username) {
         PanacheQuery<User> user = find("select u from User u where u.username = :username", Parameters.with("username", username).map());
+        try {
+            return user.singleResult();
+        } catch(NoResultException ex) {
+            return null;
+        }
+    }
+
+    public User findByEmail(String email) {
+        EmailAddress emailAddress = new EmailAddress();
+        emailAddress.setEmail( email );
+
+        PanacheQuery<User> user = find("select u from User u where u.email = :emailAddress", Parameters.with("emailAddress", emailAddress).map());
         try {
             return user.singleResult();
         } catch(NoResultException ex) {
