b2c0b258aca6b5de6026154299548221039cec9d
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-12 12:59:27 +0200

    Update user3 on import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -6,7 +6,7 @@ delete from users;
 
 insert into Users (id, username, password, the_day, the_month, the_year, email) values (1000, 'user1', 'password1', 1, 1, 1980,'user1@email.com');
 insert into Users (id, username, password, the_day, the_month, the_year, email) values (1001, 'user2', 'password2', 1, 1, 1980, 'user2@email.com');
-insert into Users (id, username, password, the_day, the_month, the_year, email) values (2002, 'user3', 'password3', 1, 1, 1980, 'user3@email.com');
+insert into Users (id, username, password, the_day, the_month, the_year, email) values (1002, 'user3', 'password3', 1, 1, 1980, 'user3@email.com');
 
 --insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, postId) values (1, 'TWEET', '2023-03-10T13:03:10.7278844', 50, 'This is tweet #1', 1, null);
 --insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, postId) values (2, 'TWEET', '2023-03-10T14:03:10.7278844', 50, 'This is tweet #2', 2, null);
