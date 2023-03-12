543681239a5f14e696719d399e849a1dcde17b7d
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-12 15:23:44 +0200

    Add PostRepresentation

---
+++ src/main/java/com/twitter/representation/RetweetRepresentation.java
@@ -0,0 +1,9 @@
+package com.twitter.representation;
+
+import java.time.LocalDateTime;
+
+public class RetweetRepresentation {
+    public Integer id;
+    public LocalDateTime dateTime;
+    public PostRepresentation parentPost;
+}
