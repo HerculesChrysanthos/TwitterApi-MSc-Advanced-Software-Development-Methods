be2b8e285ed480648fbdbfb6a3a11dca005e9254
Nikolas Lep <nikolaslep_@hotmail.com>
2023-03-16 00:06:31 +0200

    Improved import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,5 @@
 delete from following_users;
 delete from likes;
-delete from posts_posts;
 delete from posts;
 delete from users;
 
@@ -8,9 +7,9 @@ insert into Users (id, username, password, the_day, the_month, the_year, email) 
 insert into Users (id, username, password, the_day, the_month, the_year, email) values (1001, 'user2', 'password2', 1, 1, 1980, 'user2@email.com');
 insert into Users (id, username, password, the_day, the_month, the_year, email) values (1002, 'user3', 'password3', 1, 1, 1980, 'user3@email.com');
 
-insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, postId) values (1000, 'TWEET', '2023-03-10T13:03:10.7278844', 50, 'This is tweet #1000', 1000, null);
-insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, postId) values (2000, 'REPLY', '2023-03-10T13:03:10.7278844', 50, 'This is reply #2000 to tweet #1000.', 1000, 1000);
-insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, postId) values (3000, 'RETWEET', '2023-03-10T13:03:10.7278844', 50, 'This is retweet #3000 to tweet #1000.', 1000, 1000);
+insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, parentPostId, OriginalPostId) values (1000, 'TWEET', '2023-03-10T13:03:10.7278844', 50, 'This is tweet #1000', 1000, null, null);
+insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, parentPostId, OriginalPostId) values (2000, 'REPLY', '2023-03-10T13:03:10.7278844', 50, 'This is reply #2000 to tweet #1000.', 1000, 1000, null);
+insert into Posts (id, postType, createdAt, maxNumOfChars, tweetContent, userId, parentPostId, OriginalPostId) values (3000, 'RETWEET', '2023-03-10T13:03:10.7278844', null, null, 1000, null, 1000);
 
 insert into Likes (postId, userId) values (1000, 1001); -- user with id 1001 likes post 1000
 insert into Likes (postId, userId) values (2000, 1001);
