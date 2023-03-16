d269da434789f59133972a0c32d79e954bfac6a6
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-16 02:23:25 +0200

    Add not existing user id

--- src/test/java/com/twitter/Fixture.java
+++ src/test/java/com/twitter/Fixture.java
@@ -5,6 +5,7 @@ public class Fixture {
         public static final int USER1_ID = 1000;
         public static final int USER2_ID = 1001;
         public static final int USER3_ID = 1002;
+        public static final int USER4_ID = 1003;
         public static final int COUNT = 3;
     }
     public static String API_ROOT  = "http://localhost:8081";
