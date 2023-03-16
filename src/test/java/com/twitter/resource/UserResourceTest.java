6bec657582f0c70c79bed1deeb58a718eccae8be
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-16 03:02:24 +0200

    Add canNotCreateUserWithInvalidEmail test

--- src/test/java/com/twitter/resource/UserResourceTest.java
+++ src/test/java/com/twitter/resource/UserResourceTest.java
@@ -129,5 +129,27 @@ public class UserResourceTest extends IntegrationBase {
         Assertions.assertEquals("User with email 'user1@email.com' already exists.", errorResponse.getMessage());
     }
 
+    @Test
+    @TestTransaction
+    public void canNotCreateUserWithInvalidEmail() {
+        UserRepresentation user = new UserRepresentation();
+        user.username = "newUser";
+        user.password = "password";
+        user.email = "user11email.com";
+        user.dateOfBirth = new DateOfBirthRepresentation();
+        user.dateOfBirth.day = 1;
+        user.dateOfBirth.month = 1;
+        user.dateOfBirth.year = 2000;
 
+        ErrorResponse errorResponse = given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(user)
+                .when()
+                .post(Fixture.API_ROOT + TwitterUri.USERS)
+                .then()
+                .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+                .extract().as(ErrorResponse.class);
+
+        Assertions.assertEquals("Invalid email", errorResponse.getMessage());
+    }
 }
