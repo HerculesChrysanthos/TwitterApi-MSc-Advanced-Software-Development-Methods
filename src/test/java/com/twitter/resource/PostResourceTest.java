4b6b80c929bf9ae44f057cd22359857427ff6305
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-17 00:53:47 +0200

    Add findPost's tests for all of its types

--- src/test/java/com/twitter/resource/PostResourceTest.java
+++ src/test/java/com/twitter/resource/PostResourceTest.java
@@ -1,4 +1,56 @@
 package com.twitter.resource;
 
-public class PostResourceTest {
+import com.twitter.Fixture;
+import com.twitter.IntegrationBase;
+import com.twitter.representation.ReplyRepresentation;
+import com.twitter.representation.RetweetRepresentation;
+import com.twitter.representation.TweetRepresentation;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.core.Response;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+@QuarkusTest
+public class PostResourceTest  extends IntegrationBase {
+
+    @Test
+    @TestTransaction
+    public void testGetPostByIdTweetCase() {
+        TweetRepresentation tweetRepresentation = when()
+                .get(Fixture.API_ROOT + TwitterUri.POSTS + "/" + Fixture.Posts.POST1_ID)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(TweetRepresentation.class);
+
+        Assertions.assertEquals(Fixture.Posts.POST1_ID, tweetRepresentation.id);
+    }
+
+    @Test
+    @TestTransaction
+    public void testGetPostByIdReplyCase() {
+        ReplyRepresentation replyRepresentation = when()
+                .get(Fixture.API_ROOT + TwitterUri.POSTS + "/" + Fixture.Posts.POST2_ID)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(ReplyRepresentation.class);
+
+        Assertions.assertEquals(Fixture.Posts.POST2_ID, replyRepresentation.id);
+    }
+
+    @Test
+    @TestTransaction
+    public void testGetPostByIdRetweetCase() {
+        RetweetRepresentation replyRepresentation = when()
+                .get(Fixture.API_ROOT + TwitterUri.POSTS + "/" + Fixture.Posts.POST3_ID)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().as(RetweetRepresentation.class);
+
+        Assertions.assertEquals(Fixture.Posts.POST3_ID, replyRepresentation.id);
+    }
 }
