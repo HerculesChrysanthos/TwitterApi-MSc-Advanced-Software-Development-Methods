c977cacab265bfea54510e87dc662b9562cd88e7
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-17 00:51:12 +0200

    Add Posts ids on Fixture

--- src/test/java/com/twitter/Fixture.java
+++ src/test/java/com/twitter/Fixture.java
@@ -9,4 +9,11 @@ public class Fixture {
         public static final int COUNT = 3;
     }
     public static String API_ROOT  = "http://localhost:8081";
+
+    public static class Posts {
+        public static final int POST1_ID = 1000;
+        public static final int POST2_ID = 2000;
+        public static final int POST3_ID = 3000;
+        public static final int POST4_ID = 4000;
+    }
 }
