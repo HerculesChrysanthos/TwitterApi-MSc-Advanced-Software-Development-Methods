aaf455c01cdff6e4f761a8c43f2d0eaae0f61e92
Hercules Chrysanthos <hercsma@hotmail.com>
2023-03-19 22:43:42 +0200

    Add postsForUserTimeline

--- src/main/java/com/twitter/persistence/PostRepository.java
+++ src/main/java/com/twitter/persistence/PostRepository.java
@@ -1,10 +1,27 @@
 package com.twitter.persistence;
 
 import com.twitter.domain.Post;
+import com.twitter.domain.Tweet;
 import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+import io.quarkus.panache.common.Parameters;
+
 import javax.enterprise.context.RequestScoped;
-import javax.persistence.DiscriminatorColumn;
+import java.util.List;
 
 @RequestScoped
 public class PostRepository implements PanacheRepositoryBase<Post, Integer> {
+
+        public List<Tweet> postsForUserTimeline(Integer userId, Tweet offset, int limit) {
+            String query = "SELECT t FROM Tweet t WHERE t.user IN (SELECT f FROM User u JOIN u.following f WHERE u.id = :userId)";
+            System.out.println("MPIKA " +query);
+            if(offset != null ) {
+                query += " AND t.dateTime < :offsetDateTime";
+                query += " ORDER BY t.dateTime DESC";
+                return find(query, Parameters.with("userId", userId).and( "offsetDateTime", offset.getDateTime()).map()).page(0, limit).list();
+            }
+
+            query += " ORDER BY t.dateTime DESC";
+
+            return find(query, Parameters.with("userId", userId).map()).page(0, limit).list();
+    }
 }
